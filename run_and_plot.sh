cd1ce2f6b8f0ed74308316aaf5a87f037efd4ce3
o.sus <osus@ubuntu.ubuntu-domain>
2016-02-12 15:46:57 +0100

    adding all necessary files

---
+++ run_and_plot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd src/
+make
+cd ..
+
+# 1) run SPA
+./spa Prades_ndf.config
+
+# 2) plot output
+R --vanilla --slave --args "figures/" FALSE FALSE "ndf" <./plot.R
+
+display output/figures/SWC.png &
+
+# argument 5: output figure directory
+# argument 6: SDE?
+# argument 7: plot parameter correlations (time consuming)?
+# argument 8: defoliated or nondefoliated?
+
